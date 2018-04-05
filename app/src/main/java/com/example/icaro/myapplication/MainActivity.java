b65b3c831bb9fc2e93cc96f2e42ab06f09dcb7d6
alexandrepsjf <alexandrepsjf@hotmail.com>
2018-04-05 13:28:26 -0300

    Toast com date

--- app/src/main/java/com/example/icaro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/icaro/myapplication/MainActivity.java
@@ -1,44 +1,34 @@
 package com.example.icaro.myapplication;
 
 import android.icu.util.Calendar;
-import android.icu.util.TimeZone;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
-
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
-    MaterialCalendarView mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
+        MaterialCalendarView mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
         mcv.state().edit()
                 .setFirstDayOfWeek(Calendar.SUNDAY)
                 .setMinimumDate(CalendarDay.from(2018, 3, 3))
                 .setMaximumDate(CalendarDay.from(2019, 0, 12))
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit();
-
-
-
         mcv.setSelectedDate(CalendarDay.today().getDate());
-        String data  = mcv.getSelectedDate().getDate().toString();
-mcv.setOnDateChangedListener(new OnDateSelectedListener() {
-    @Override
-    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-        Toast.makeText(getApplicationContext(),date.toString(),Toast.LENGTH_LONG);
-    }
-});
+        mcv.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView mcv, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(getApplicationContext(), date.toString(), Toast.LENGTH_LONG);
+            }
+        });
 
 
     }
