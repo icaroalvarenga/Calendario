4b1beeb091a609b18d64547ebd33f4b8a942fe4d
icaroalvarenga <icaro_alvarenga@hotmail.com>
2018-04-11 13:18:32 -0300

    asf

--- app/src/main/java/com/example/icaro/myapplication/MainActivity.java
+++ app/src/main/java/com/example/icaro/myapplication/MainActivity.java
@@ -1,19 +1,30 @@
 package com.example.icaro.myapplication;
 
+import android.graphics.Color;
+import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
 import android.widget.Toast;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final HashSet<CalendarDay> dates = new HashSet<>();
+
+
         setContentView(R.layout.activity_main);
         MaterialCalendarView mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
         mcv.state().edit()
@@ -23,10 +34,19 @@ public class MainActivity extends AppCompatActivity {
                 .setCalendarDisplayMode(CalendarMode.MONTHS)
                 .commit();
         mcv.setSelectedDate(CalendarDay.today().getDate());
+
         mcv.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView mcv, @NonNull CalendarDay date, boolean selected) {
                 Toast.makeText(getApplicationContext(), date.toString(), Toast.LENGTH_LONG);
+                SimpleDateFormat mFormat = new SimpleDateFormat();
+                dates.add(date);
+            mcv.setBackground();
+                EventDecorator event=new EventDecorator(Color.RED,dates);
+
+
+
+                Log.d("teste",mFormat.format(date.getDate()));
             }
         });
 
